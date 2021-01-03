40eb08cca4ad3ede5b85b69188c571ec13bec915
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-01-03 15:51:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Triangle from "./make-triangle";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Triangle />
+    </div>
+  );
+}
---
+++ src/make-triangle.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Triangle() {
+  return (
+    <div className="App">
+      <h1>Triangles</h1>
+      <h2>Enter three angles below: </h2>
+      <input />
+      <input />
+      <input />
+      <h2>Result: </h2>
+    </div>
+  );
+}
