d821598e0cf6a2726942dda53dbf15dc8c06ee9f
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-01-04 20:06:02 +0530

    update: changes

--- src/make-triangle.js
+++ src/make-triangle.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Triangle() {
-  function inputClickHandler() {
-    console.log("hello");
+
+  var [result, setResult]= useState("The result will appear here")
+
+  function inputClickHandler1(event) {
+    var angleOne = event.target.value;
+    return (angleOne);
   }
+
+  function inputClickHandler2(event) {
+    var angleTwo = event.target.value;
+    return (angleTwo);
+  }
+
+  function inputClickHandler3(event) {
+    var angleThree = event.target.value;
+    return (angleThree);
+  }
+
+
+   function finalResult() {
+    var sumOfAngles = inputClickHandler1() + inputClickHandler2() + inputClickHandler3())
+  }
+
+  // console.log(
+  //   inputClickHandler1() + inputClickHandler2() + inputClickHandler3()
+  // );
 
   return (
     <div className="App">
       <h1>Triangles</h1>
       <h2>Enter three angles below to make a triangle: </h2>
-      <input onChange={inputClickHandler} />
-      <input />
-      <input />
+      <input value={"input1"} onChange={inputClickHandler1} />
+      <input value={"input2"} onChange={inputClickHandler2} />
+      <input value={"input3"} onChange={inputClickHandler3} /> 
       <h2>Result: </h2>
     </div>
   );
