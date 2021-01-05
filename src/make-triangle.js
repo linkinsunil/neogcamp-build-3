70da2da55e9726d45b55008fee00ec9bc70f3b84
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-01-06 02:03:18 +0530

    update: main function 1st task completed

--- src/make-triangle.js
+++ src/make-triangle.js
@@ -1,27 +1,38 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+
+var angleOne = 0;
+var angleTwo = 0;
+var angleThree = 0;
 
 export default function Triangle() {
-
-  var [result, setResult]= useState("The result will appear here")
+  var [result, setResult] = useState("Enter angles above and check");
+  // var [input, setInput] = useState(0)
 
   function inputClickHandler1(event) {
-    var angleOne = event.target.value;
-    return (angleOne);
+    angleOne = event.target.value;
+    return angleOne;
   }
 
   function inputClickHandler2(event) {
-    var angleTwo = event.target.value;
-    return (angleTwo);
+    angleTwo = event.target.value;
+    return angleTwo;
   }
 
   function inputClickHandler3(event) {
-    var angleThree = event.target.value;
-    return (angleThree);
+    angleThree = event.target.value;
+    return angleThree;
   }
 
-
-   function finalResult() {
-    var sumOfAngles = inputClickHandler1() + inputClickHandler2() + inputClickHandler3())
+  function buttonClickHandler() {
+    var sumOfAngles =
+      parseInt(angleOne, 10) +
+      parseInt(angleTwo, 10) +
+      parseInt(angleThree, 10);
+    if (sumOfAngles === 180) {
+      setResult("This is a triangle");
+    } else {
+      setResult("This is NOT a triangle");
+    }
   }
 
   // console.log(
@@ -32,10 +43,11 @@ export default function Triangle() {
     <div className="App">
       <h1>Triangles</h1>
       <h2>Enter three angles below to make a triangle: </h2>
-      <input value={"input1"} onChange={inputClickHandler1} />
-      <input value={"input2"} onChange={inputClickHandler2} />
-      <input value={"input3"} onChange={inputClickHandler3} /> 
-      <h2>Result: </h2>
+      <input onChange={inputClickHandler1} />
+      <input onChange={inputClickHandler2} />
+      <input onChange={inputClickHandler3} />
+      <button onClick={buttonClickHandler}> Check </button>
+      <h2>Result: {result} </h2>
     </div>
   );
 }
